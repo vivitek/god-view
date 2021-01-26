ebc116f269e02ee7c78dce18d771c7d8258dae00
Théo MARINHO <theo.marinho@epitech.eu>
2021-01-26 10:24:16 +0100

    [add] add router listing basement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Header from './components/Header/Header'
 import Router from './Router'
 import './App.css';
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+import { ApolloProvider } from '@apollo/client';
+import { client } from './graphql';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
-        <Router />
+          <ApolloProvider client={client}>
+            <Router />
+          </ApolloProvider>
       </BrowserRouter>
     </div>
   );
--- src/pages/database/Database.js
+++ src/pages/database/Database.js
@@ -1,9 +1,56 @@
 import './Database.css'
 import '../Page.css'
+import { GET_ROUTERS } from "./queries/router"
+import { useQuery } from '@apollo/client'
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TablePagination
+} from "@material-ui/core"
 
-function Database() {
+const Database = () => {
+  const {loading, error, data} = useQuery(GET_ROUTERS)
+
+  if (loading) return <div className="page"> Loading...</div>
+  if (error) return <div className="page"> Error, please contact Matteo</div>
+
   return (
-    <div className="page">This is the database page</div>
+    <div className="page main">
+      <div className="card">
+        <TableContainer>
+          <Table size="medium">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                <TableCell>name</TableCell>
+                <TableCell>url</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.getRouters.map(router => {
+                  return (
+                    <TableRow key={router.id}>
+                      <TableCell>{router._id}</TableCell>
+                      <TableCell>{router.name}</TableCell>
+                      <TableCell>{router.url}</TableCell>
+                    </TableRow>
+                  )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div className="card">user</div>
+      <div className="card">config</div>
+      <div className="card">ban</div>
+      {
+        console.log(data.getRouters)
+      }
+    </div>
   )
 }
 
