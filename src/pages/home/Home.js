9ab32face236309e06747e541d3f4f774ded17f0
Dythic98 <quentin.henry@epitech.eu>
2021-01-27 21:47:13 +0100

    ADD beginning homePage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,132 @@
 import './Home.css'
 import '../Page.css'
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 function Home() {
+
   return (
-    <div className="page">This is the home page</div>
-  )
+    <div className="page main">
+      <div className="card">
+      <Grid container spacing={2}>
+          <Grid item>
+            <Button variant="contained" color="secondary">
+              Secondary
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" color="primary">
+                  Server API ViVi
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="card">
+      <Grid container spacing={2}>
+          <Grid item>
+            <Button variant="contained" color="secondary">
+              Secondary
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" color="primary">
+                  Server API ViVi
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="card">
+      <Grid container spacing={2}>
+          <Grid item>
+            <Button variant="contained" color="secondary">
+              Secondary
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" color="primary">
+                  Server API ViVi
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+      <div className="card">
+      <Grid container spacing={2}>
+          <Grid item>
+            <Button variant="contained" color="secondary">
+              Secondary
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" color="primary">
+                  Server API ViVi
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Full resolution 1920x1080 • JPEG
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default Home
+
