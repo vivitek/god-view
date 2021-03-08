84583e3677a1d58f06fea54d06a82fc68030ab37
Théo MARINHO <theo.marinho@epitech.eu>
2021-03-08 12:28:48 +0100

    [fix] fix env variable

--- src/pages/terminal/Terminal.js
+++ src/pages/terminal/Terminal.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import './Terminal.css'
 import '../Page.css'
 
-const socket = io(process.env.ENDPOINT).connect()
+const socket = io(process.env.REACT_APP_ENDPOINT).connect()
 
 const Terminal = () => {
   const [commandList, setCommandList] = useState([])
