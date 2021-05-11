925ce32bdc7152e8fad2aa8b260c8ed0f4752bff
Dythic98 <quentin.henry@epitech.eu>
2021-05-11 12:29:52 +0200

    [remove] Rome useState and useEffect in Stat.js

--- src/pages/stats/Stat.js
+++ src/pages/stats/Stat.js
@@ -1,6 +1,6 @@
 import './Stat.css'
 import '../Page.css'
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
