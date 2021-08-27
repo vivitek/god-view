b2844610c413a0e50b36882bfc4dddfbc5f93c53
Theo Marinho <theo.marinho@epitech.eu>
2021-08-27 17:41:06 +0200

    [add] add logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,21 @@ const Header = () => {
 
     return (
     <header className="z-0">
-      <div
-        className="w-full bg-darkBlue h-12 md:h-20 text-white flex items-center"
-      >
-        <Link to="/">
-          <ViviHourglass className="m-3 h-6 w-auto" />
+      <div className="w-full bg-darkBlue h-12 md:h-20 text-white flex  justify-between items-center">
+        <div className="flex flex-row items-center">
+          <Link to="/">
+            <ViviHourglass className="m-3 ml-4 h-6 w-auto" />
+          </Link>
+          <Link to="/box">Boxes</Link>
+          <Link className="ml-4" to="/statistics">Statistics</Link>
+          <Link className="ml-4" to="/database">Database</Link>
+        </div>
+        <Link className="mr-4" to="/login" onClick={() => {
+          localStorage.removeItem('VIVI_godview_token')
+          localStorage.removeItem('VIVI_godview_user')
+        }}>
+          <img className="h-6 w-auto" src="/logout.png" alt="Logout" />
         </Link>
-        <Link className="ml-4" to="/box">Boxes</Link>
-        <Link className="ml-4" to="/statistics">Statistics</Link>
-        <Link className="ml-4" to="/database">Database</Link>
       </div>
     </header>
   );
